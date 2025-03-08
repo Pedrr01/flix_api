d2340e2d6d341c2ede94460a7c9923190047322b
Pedro Vitor <pedrovitor.silva14vs@gmail.com>
2025-03-07 21:21:02 -0300

    add post and get

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_list_view
+from genres.views import genre_create_list_view, genre_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_list_view, name='genre_view')
+    path('genres/', genre_create_list_view, name='genre-create-list-view'),
+    path('genres/<int:pk>/',genre_detail_view ,name='genre-detail-view'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from genres.models import Genre
 from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 
-def genre_list_view(request):
-    genres = Genre.objects.all()
-    data = [{'id' : genre.id, 'name': genre.name} for genre in genres]
-    return JsonResponse(data, safe=False)
+@csrf_exempt
+def genre_create_list_view(request):
+    if request.method =='GET':
+        genres = Genre.objects.all()
+        data = [{'id' : genre.id, 'name': genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data['name'])
+        new_genre.save()
+        return JsonResponse({'id' : new_genre.id, 'name': new_genre.name}, status=201,)
+
+@csrf_exempt
+def genre_detail_view(requeste, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    data = {'id': genre.id, 'name': genre.name}
+    return JsonResponse(data)
