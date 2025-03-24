1bfa0a923bfa3bb1fa511c6506cdf53585ae4730
Pedro Vitor <pedrovitor.silva14vs@gmail.com>
2025-03-23 21:01:47 -0300

    creating the serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from movies.models import Movie
 
-class MovieSerializers(serializers.ModelSerializer):
+class MovieModelSerializers(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        return 5
+
+    def validate_release_date(self, value):
+        if value.year <= 2000:
+            raise serializers.ValidationError('ERRO! Só aceitamos filmes a parti do ano 2000.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('ERRO! Resumo deve ter menos de 200 catacteres.')
+
--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,14 @@
 from rest_framework import generics
 from movies.models import Movie
-from movies.serializers import MovieSerializers
+from movies.serializers import MovieModelSerializers
 
 class MovieCreateListView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializers
+    serializer_class = MovieModelSerializers
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializers
+    serializer_class = MovieModelSerializers
 
 
 
